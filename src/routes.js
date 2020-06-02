4192486096fff5a8381657defd73c0bbcf1e4622
djkajo <carlosserraamorim@gmail.com>
2020-06-02 07:01:48 +0100

    routes corrected

--- src/routes.js
+++ src/routes.js
@@ -2,7 +2,6 @@ import React from 'react';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import Text from "./pages/text/text";
 import Instru from "./pages/instructions/instru";
-import About from "./pages/about/about";
 
 function Routes () {
     return (
